e5b85ff3b809a94e339239e67b11bbb55206ef36
Bill Mills <bill.mills@linaro.org>
2025-10-30 23:13:54 -0400

    5050A: adjust test.sh for actual demo
    * Do 3 boot phases
      1) Pre-boot to fix hang issue
      2) Real Arm boot and qemu start
      3) warm boot so x86 can start fresh w/ arm ready
    * Test ping originating from both sides
    * In test-loop.sh add date/time to logs
    * Ignore the results.txt symlink/file if it exists
    
    Signed-off-by: Bill Mills <bill.mills@linaro.org>

--- 5050A/jtag-boot/test-loop.sh
+++ 5050A/jtag-boot/test-loop.sh
@@ -7,11 +7,17 @@ if [ -n "$1" ]; then
     NUM=$1
 fi
 
+human_date() {
+    echo $(date +"%Y/%m/%d %H:%M:%S")
+}
+
 rm -f results-$NOW.txt
 touch results-$NOW.txt
 ln -sf results-$NOW.txt results.txt
 
 for i in $(seq 1 $NUM); do
-    echo "test # $i" | tee -a results.txt
+    echo "test # $i at $(human_date)" | tee -a results.txt
     ./test.sh;
 done
+
+echo "all tests done at $(human_date)" | tee -a results.txt
--- 5050A/jtag-boot/test.sh
+++ 5050A/jtag-boot/test.sh
@@ -2,35 +2,54 @@
 
 source common.sh
 
-# try from cold boot
+# pre-boot
 boardctl vpr_5050A off
 echo "wait in poweroff"
 sleep 20
 boardctl vpr_5050A on
 wait_ssh vpr-5050A
-ssh vpr-5050A v2/load.sh mmc
-echo "wait for arm boot"
-sleep 40
-# ping arm, won't work
-test_ping $ARM_IP "from cold start" | tee -a results.txt
+ssh vpr-5050A v2/load.sh none
+echo "wait for u-boot to run"
+sleep 10
 
-
-# try from warm boot
+# now do warm boot so arm can boot all the way
 ssh -t vpr-5050A sudo shutdown -r now
 echo "wait for shutdown to finish"
 sleep 20
 wait_ssh vpr-5050A
 ssh vpr-5050A v2/load.sh mmc
 echo "wait for arm boot"
-sleep 40
-# ping arm, should work
-test_ping $ARM_IP "from warm start" | tee -a results.txt
-test_ssh vpr-5050A-arm "from warm start" | tee -a results.txt
+wait_ssh vpr-5050A-arm
+echo "Start QEMU on arm"
+ssh vpr-5050A-arm ./run-versal-virtio-msg-net-backend.sh
+echo "Wait for debug"
+sleep 10
+
+# warm boot so x86 can get in sync
+ssh -t vpr-5050A sudo shutdown -r now
+echo "wait for shutdown to finish"
+sleep 20
+wait_ssh vpr-5050A
+echo "Start X86 drivers"
+ssh -t vpr-5050A ./run-x86-virtio-msg-net-frontend.sh
+
+if ssh vpr-5050A ping -c 3 192.168.17.1; then
+    echo "Ping from x86 to arm OK" | tee -a results.txt
+else
+    echo "Ping from x86 to arm FAILED" | tee -a results.txt
+fi
+
+if ssh vpr-5050A-arm ping -c 3 192.168.17.2; then
+    echo "Ping from arm to x86 OK" | tee -a results.txt
+else
+    echo "Ping from arm to x86 FAILED" | tee -a results.txt
+fi
 
 # clean power off for next time
+ssh vpr-5050A-arm poweroff
 ssh -t vpr-5050A sudo shutdown now
 echo "wait for shutdown to finish"
-sleep 30
+sleep 20
 boardctl vpr_5050A off
 
 
